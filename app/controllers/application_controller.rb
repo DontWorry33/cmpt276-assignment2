5e40ae0fcc628ba35b838ce24f4a49ccbc7ae91a
Hussain Hassam <hussain.hassam21@gmail.com>
2016-10-10 11:15:22 -0700

    added navigation bar with links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :set_total_tokimon
+  def set_total_tokimon
+    @totalTokimons = Tokimon.all.length
+  end
+  
 end
